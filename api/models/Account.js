5297fb30e1c375c831768d343944c7e0a6086e7e
thomastiamlee <thomasjamestiamlee@gmail.com>
2018-06-05 14:16:04 +0900

    Fixed user ID data type

--- api/models/Account.js
+++ api/models/Account.js
@@ -2,7 +2,7 @@ module.exports = {
 	attributes: {
 		email: { type: 'string', required: true },
 		password: { type: 'string', required: true },
-		user_id: { type: 'string', required: true },
+		user_id: { type: 'number', required: true },
 		region: { type: 'string', required: true },
 		group: {type: 'string', required: true },
 		syntax_solved: { type: 'number', required: true },
