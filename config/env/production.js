0436af7dafbb8b078c76a105ddc356001416af3c
thomastiamlee <thomasjamestiamlee@gmail.com>
2018-06-06 15:59:21 +0900

    Updated information.

--- config/env/production.js
+++ config/env/production.js
@@ -184,8 +184,8 @@ module.exports = {
     * > (For a full list, see https://sailsjs.com/plugins/sessions)            *
     *                                                                          *
     ***************************************************************************/
-      adapter: 'connect-mongo',
-      url: 'mongodb://localhost:27017/session',
+    //  adapter: 'connect-mongo',
+    //  url: 'mongodb://localhost:27017/session',
     //--------------------------------------------------------------------------
     // /\   OR, to avoid checking it in to version control, you might opt to
     // ||   set sensitive credentials like this using an environment variable.
